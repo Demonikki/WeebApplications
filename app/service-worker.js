118bec077806e4c4d9a0b165b12c85dbff6ba623
Demonikki <nsankara@ucsd.edu>
2017-08-02 14:42:08 -0700

    basic offline

---
+++ app/service-worker.js
@@ -0,0 +1,40 @@
+var cacheVersion = 1;
+var currentCache = {
+  offline: 'offline-cache' + cacheVersion
+};
+
+const offlineUrl = 'offline.html';
+const offlineImg = '/media/error-img.jpg';
+var filesToCache = [offlineUrl, offlineImg];
+ self.addEventListener('install', function(event) {
+     //Setup cache
+    event.waitUntil(
+        caches.open(currentCache.offline)
+          .then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(filesToCache);
+            console.log('Added files to cache');
+        })
+    );
+ });
+
+ this.addEventListener('fetch', event => {
+  // request.mode = navigate isn't supported in all browsers
+  // so include a check for Accept: text/html header.
+  if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
+        event.respondWith(
+          fetch(event.request.url).catch(error => {
+              // Return the offline page
+              return caches.match(offlineUrl);
+          })
+    );
+  }
+  else{
+        // Respond with everything else if we can
+        event.respondWith(caches.match(event.request)
+                        .then(function (response) {
+                        return response || fetch(event.request);
+                    })
+            );
+      }
+});
